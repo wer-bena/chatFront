7fb192dc9f40017dbc0abb27b877488700fad8dc
Bernadeta G <grudzien.bernadeta@example.com>
2021-04-11 11:03:10 +0200

    Registration component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainPageComponent} from './main-page/main-page.component';
+import {RegisterComponent} from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
